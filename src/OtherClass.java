5e0f1effbe6a87c49a76f64171fa3d3fa4fca8d0
JereHu <34283813+JereHu@users.noreply.github.com>
2019-10-08 12:51:51 +0200

    Another one2

--- src/OtherClass.java
+++ src/OtherClass.java
@@ -1,8 +1,14 @@
 public class OtherClass {
     public static void main(String[] args){
-        SecondTest myObj = new SecondTest();
+        SecondTest myObj = new SecondTest(9);
         myObj.hello();
         SecondTest.example();
+
+        SecondTest myObj2 = new SecondTest(8);
+        myObj2.x = 300;
+
+        System.out.println(myObj2.z);
+
     }
 }
 
--- src/WhyNot.java
+++ src/WhyNot.java
@@ -1,5 +1,5 @@
 public class WhyNot {
     public static void main(String[] args){
-        System.out.println("WhyNot")
+        System.out.println("WhyNot");
     }
 }
